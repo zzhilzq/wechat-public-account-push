64b37ab74cda491baa6ff1d688d7753db857a3be
zzhilzq <142958867+zzhilzq@users.noreply.github.com>
2023-08-23 11:34:01 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -44,7 +44,7 @@ const USER_CONFIG = {
         // 在一起的日子
         { keyword: 'love_day', date: '2023-08-07' },
         // 结婚纪念日
-        //{ keyword: 'marry_day', date: '2022-09-09' },
+        { keyword: 'marry_day', date: '2022-09-09' },
       ],
     },
   ],
